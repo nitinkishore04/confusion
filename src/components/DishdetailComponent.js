61c15c6e53755a07aa7dd0d9be52f0a901ff6b9c
Nitin Kishore <nitin.kishore04@gmail.com>
2020-06-18 18:40:01 +0530

    Before Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if( dish != null) {
@@ -21,10 +22,9 @@ import { Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
             );
         }
     }
-    function RenderComment({dish}) {
-        if (dish != null) {
-
-                const cmnt = dish.comments.map(comment => {
+    function RenderComment({comments}) {
+        if (comments != null) {
+                const cmnt = comments.map((comment) => {
                     return (
                         <div className="m-2">
                             <p>{comment.comment}</p>
@@ -52,13 +52,24 @@ import { Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
     }
     function Dishdetail (props) {
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish}/>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComment dish={props.dish}/>
-                </div>
+            <div className="container">
+                <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                 </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish}/>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComment comments={props.comments}/>
+                    </div>
+            </div>
             </div>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes'
 import { LEADERS } from '../shared/leaders'
@@ -31,6 +32,14 @@ class Main extends Component {
       );
     }
 
+  const DishWithId = ({match}) => {
+    return (
+      <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+      />
+    );
+  }
+
 
     return (
       <div>
@@ -38,7 +47,8 @@ class Main extends Component {
             <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route exact path='/contactus' component={Contact}/>
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/contactus' component={ Contact }/>
               <Redirect to="/home" />
             </Switch>
             <Footer/>
