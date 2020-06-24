e6159846a3fce7c235790fda1b13295ccf2f9587
Nitin Kishore <nitin.kishore04@gmail.com>
2020-06-24 22:11:09 +0530

    Task 1 Assignment 4

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -145,3 +145,42 @@ export const addPromos = (promos) => ({
     type: ActionType.ADD_PROMOS,
     payload: promos
 });
+
+
+
+export const fetchLeaders = () => (dispatch) => {
+  dispatch(leadersLoading(true));
+  
+  
+  return fetch(baseUrl + 'leaders')
+      .then(response => {
+          if (response.ok) {
+          return response;
+          } 
+          else {
+              var error = new Error('Error ' + response.status + ': ' + response.statusText);
+              error.response = response;
+              throw error;
+          }
+      },
+      error => {
+          var errmess = new Error(error.message);
+          throw errmess;
+      })
+      .then(response => response.json())
+      .then(leaders => dispatch(addLeaders(leaders)))
+      .catch(error => dispatch(leadersFailed(error.message)));
+}
+export const leadersLoading = () => ({
+  type: ActionType.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+  type: ActionType.LEADERS_FAILED,
+  payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+  type: ActionType.ADD_LEADERS,
+  payload: leaders
+});
